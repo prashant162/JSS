fa3a251e40ba139256f9da64c611ac66af6b36ad
Prashant Tomar <prashant.tomar@hotmail.com>
2019-12-17 15:39:47 +0530

    completed the navigation module

--- my-first-jss-app/src/components/Navigation/index.js
+++ my-first-jss-app/src/components/Navigation/index.js
@@ -3,8 +3,13 @@ import { Link } from '@sitecore-jss/sitecore-jss-react';
 
 const Navigation = (props) => (
     <div>
-        <p>Navigation Component</p>
-        <Link field={props.fields.path} />
+        <br/>
+        <h1>Navigation Component</h1>
+        <ul>
+            {props.fields.childPages.map((childPage, index) => {
+                return <li key={index}><Link field={childPage} /></li>
+            })}
+        </ul>
     </div>
 );
 
